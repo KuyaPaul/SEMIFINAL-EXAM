c134417e5bf88728519c663ffdc64f8acecd2549
unknown <Mark John Paul A. De Ocampo>
2022-05-28 14:15:29 +0800

    This is the created new models in this project

--- FoodWareHouseAPI/models.py
+++ FoodWareHouseAPI/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class foods(model.Models):
+    Mfd_id = models.IntegerField(primary_key=True)
+    Name = models.CharField(max_length=50)
+    unitPrice = models.DecimalField(max_digits=5, decimal_places=2)
+    unitStock = models.PositiveSmallIntegerField
+
+    def __str__(self):
+        return self.Name
